f224de746d15e0c7a4a91fd072b24aa7ad8d87b5
Marcelo Sadaike <msadaike@scopus.com.br>
2019-04-03 14:08:30 -0300

    Versão de teste 1.0 do YoloV2

---
+++ MainSinglePhoto.py
@@ -0,0 +1,16 @@
+import cv2
+from darkflow.net.build import TFNet
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    #%config InlineBackend.figure_format = 'svg'
+    '''
+    options = {
+        'model': 'cfg/yolo.cfg',
+        'load': 'bin/yolov2.weights',
+        'threshold': 0.3,
+        #'gpu': 0.7
+    }
+
+    tfnet = TFNet(options)
+    '''
